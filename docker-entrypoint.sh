1ce7e3dc15f299a376405fc14d2d294c3c386a3b
qiuzi <gxfclql@gmail.com>
2022-07-28 22:30:23 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,18 +1,19 @@
 #! /bin/bash -eu
-#cat $CONFIG_FILE >config/.config.php
-# vendor/bin/phinx migrate && \
-# php xcat Tool importAllSettings
-# php xcat Tool detectConfigs
-# php xcat User createAdmin <<EOF
-# $ADMINEMAIL
-# $ADMIMNPASSWD 
-# Y
-# EOF
-# php xcat Tool initQQwry
+echo $CONFIG_FILE > config/.config.php
+if [ "$DATABASE" = "yse" ]; then
+ vendor/bin/phinx migrate && \
+ php xcat Tool importAllSettings
+ php xcat Tool detectConfigs
+ php xcat User createAdmin <<EOF
+ $ADMINEMAIL
+ $ADMIMNPASSWD 
+ Y
+ EOF
+ php xcat Tool initQQwry
+fi
 
 envsubst '$PORT' < /nginx.conf.template > /etc/nginx/nginx.conf
 
 cat /etc/nginx/nginx.conf
 
 supervisord -c /supervisord.conf
-
